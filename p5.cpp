ab9687e590e6dc23664bce22e90d002877907f8a
Aditya Patil <adityapatil8828@gmail.com>
2026-08-06 22:49:03 +0530

    reversing  number but  with different loop

--- p5.cpp
+++ p5.cpp
@@ -4,11 +4,11 @@ int main(){
     int n,rev=0,last;
     cout<<"enter the number:";
     cin>>n;
-    for(;n>0;){
+    do{
         last=n%10;
         rev=rev*10+last;
         n=n/10;
-    }
+    }while(n>0);
     cout<<"the reversed number is:"<<rev;
     return 0;
 }
