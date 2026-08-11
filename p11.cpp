c98e73f0c77e45858ffb217c82053ac21d29eb9d
Aditya Patil <adityapatil8828@gmail.com>
2026-08-11 23:51:39 +0530

    pattern of stars for hollow square

---
+++ p11.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    cout<<"enter the size of the matrix(nxn):";
+    cin>>n;
+    for(i=1;i<=n;++i){
+        for(j=1;j<=n;++j){
+            if(i==1||i==n||j==n||j==1){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
