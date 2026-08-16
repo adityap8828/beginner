73d7c09604e7aa9da7972cd895b9522bc2266307
Aditya Patil <adityapatil8828@gmail.com>
2026-08-16 17:06:10 +0530

    factorial of a number

---
+++ factorial.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,fact=1;
+    cout<<"enter the number:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        fact*=i;
+    }
+        cout<<"the factorial of"<<n<<"is:"<<fact;
+    return 0;
+}
