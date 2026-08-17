b9d47fec79130e39474d13f880bc8b61f2cabb8c
Aditya Patil <adityapatil8828@gmail.com>
2026-08-17 19:48:14 +0530

    finding prime numbers using udf

--- primeudf.cpp
+++ primeudf.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 using namespace std;
 void primenum(int n){
-    for(int i=0;i<=n;i++)
-    if(n<0){
-        cout<<"enter a positive number:";
-    }
-    else if(i%n==0){
+    if(n<=1){
         cout<<"the number is not a prime number.";
+        return;
     }
-    else
+    for(int i=2;i<n;i++){
+    if(n%i==0){
+        cout<<"the number is not a prime number.";
+        return;
+    }
+}
+
     cout<<"the number is a prime number.";
+    
 
 }
 int main(){
