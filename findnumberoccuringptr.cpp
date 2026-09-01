f9de2b2f7d808b67b622ec5ecaad857b097768aa
Aditya Patil <adityapatil8828@gmail.com>
2026-09-01 20:38:28 +0530

    this is the real code as the previous was just taking the inputs

--- findnumberoccuringptr.cpp
+++ findnumberoccuringptr.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
     int n,key;
+    int count=0;
     cout<<"enter the size of array:";
     cin>>n;
     int arr[n];
@@ -9,7 +10,23 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    cout<<"the array elements are={"<<arr[n]<<"}";
+    cout<<"the array elements are=";
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+
+    }
+    cout<<"enter the number to find:";
+    cin>>key;
+    int *ptr=arr;
+    for (int i=0;i<n;i++){
+        if(*(ptr+i)==key){
+            count++;
+        }
+
+    }
+    cout<<"the number of times "<<key<<" occured is "<<count<<" times.";
+    return 0;
+    
 
 
 }
