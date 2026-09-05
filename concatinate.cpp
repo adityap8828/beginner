4da2e89ef2be85b56d7b7a658a8493bec016a10c
Aditya Patil <adityapatil8828@gmail.com>
2026-09-05 20:11:48 +0530

    addding two strings

---
+++ concatinate.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+int main(){
+    string s1,s2;
+    cout<<"enter the first string:";
+    getline(cin,s1);
+    cout<<"enter the second string:";
+    getline(cin,s2);
+    cout<<"after concattination both string becomes:"<<s1+s2;
+    return 0;
+}
